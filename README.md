d7d5e79ae9f1125fb02830fa61de7a48f58ebf20
steveruu <prchalek@gmail.com>
2022-11-19 21:27:25 +0100

    bro's working atm

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 <img src="assets\spotify_logo.png" alt="Spotify Logo" width="25" height="25"> Spotify Web Scraper
 =======================
 
-Spotify Web Scraper crawls through the Spotify web interface and extracts artist logo information.
+Spotify Web Scraper crawls through the Spotify web interface and extracts artist monthly listener information.
 
 ## Technology Stack
  - [Beautiful Soup](https://pypi.org/project/beautifulsoup4/) - Parse HTML code
- - [PyMongo](https://api.mongodb.com/python/current/#) - Python MongoDB Driver
  - [Selenium](https://pypi.org/project/selenium/) - Browser Automation Tool
 
 ## Dependencies
@@ -13,7 +12,6 @@ Install Dependencies with [pip](https://pypi.org/project/pip/)
 
 ```
 pip install beautifulsoup4
-pip install pymongo
 pip install selenium
 ```
 
@@ -22,23 +20,11 @@ Alternatively you can install the requirement.txt
 pip install -r requirement.txt
 ```
 
-## Configuration Setup
-The scraper will look for **config.py** file at the root with the following parameters to connect to your database
-
-```python
-# config.py
-
-DATABASE_CONFIG = {
-    'host': 'mongodb://{0}:{1}@MONGODB_HOST/DB_COLLECTION_NAME',
-    'dbuser': '', # Database user
-    'dbuserpassword': '', # Database password
-}
-```
-
 ## Selenium Chrome Webdriver
 Since Spotify webpages are generated dynamically, we need a headless browser to generate the webpages dynamically from the source. I have chosen the Chrome WebDriver which works great in this instance.
 
 Download and save the <a href="http://chromedriver.chromium.org/downloads">Chrome WebDriver</a> at the root level.
 
-## Author
-  [Chris Yang](https://chrisyang.io) (ServiceNow Architect, GlideFast)
+## Authors
+  Author of original repo - [Chris Yang](https://chrisyang.io) (ServiceNow Architect, GlideFast)
+  Fork Author - [Steveruu](https://steveruu.github.io) (idiot)
--- scraper.py
+++ scraper.py
@@ -12,9 +12,55 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(executable_path=r'chromedriver.exe', chrome_options=chrome_options)
 
-# Set an initial artist seed, I've used my favorite artist Giuseppe Ottaviani
-seed = "/artist/4NOFcRCgjvnRy8nKVGUM0L"
+artists = ["4NOFcRCgjvnRy8nKVGUM0L", # steveruu
+                "1NspLfgAsucc39MeTipXNy", # car
+                "3GuGHOzPZ0AhH9hK8LqCsK", # pepap
+                "2aZD8xH5DKRUwAR6mXAifV", # hakaslakas
+                "3xVvsXvpURgj3zeTYiBtCv", # valian
+                "2b5QC4KWCMRKdD7LiqvfMQ", # faon
+                "50ENuvgRkFZ5hMA0BFEeAM", # helena
+                "2qSLwqeQFUHWEzC86u3vRM", # awoken
+                "6kEMNp6TPPl70gOicGT0uN", # děcko acid
+                "3D57Cu0cu9caAvtl41xUx6", # samzel
+                "5QvicxsGxXNicXu1f9guia", # yui paly
+                "70Cg3NKGzk0G16trbrfYE5", # maria
+                "1HbkAuG6cZndTXlORaQgOq", # ubránek
+                "3tjBt96Yk1zS14xc8wldlT", # fembo ypl a
+                "5NTcWbyHYQjA20voWilXeG", # kila asky
+                "7h22ZneYwwRyOwlgnMd8So", # akarlos
+                "5PNDGjJ1e6Tdr8LWmZDqPO", # ne duch
+                "0IM0lwjzI0BYaayMweraKT", # matmej
+                "34YDbjVGCySBRPAS19xl1L", # paply adomi
+                "2IIf5hkbIzh1dqhG1T132E", # krobra08
+                "3TTWuZxamiQERzR42VNMS5", # sopka
+                "6UIdgISBaIHMOvWwz4nfP1", # prasak
+                "569eihmWcdg4HvSPDnjlPn" # ondredaj
+                ]
 
+# Set an initial artist seed, I've used my favorite artist Giuseppe Ottaviani
+seed0 = "/artist/" + artists[0]
+seed1 = "/artist/" + artists[1]
+seed2 = "/artist/" + artists[2]
+seed3 = "/artist/" + artists[3]
+seed4 = "/artist/" + artists[4]
+seed5 = "/artist/" + artists[5]
+seed6 = "/artist/" + artists[6]
+seed7 = "/artist/" + artists[7]
+seed8 = "/artist/" + artists[8]
+seed9 = "/artist/" + artists[9]
+seed10 = "/artist/" + artists[10]
+seed11 = "/artist/" + artists[11]
+seed12 = "/artist/" + artists[12]
+seed13 = "/artist/" + artists[13]
+seed14 = "/artist/" + artists[14]
+seed15 = "/artist/" + artists[15]
+seed16 = "/artist/" + artists[16]
+seed17 = "/artist/" + artists[17]
+seed18 = "/artist/" + artists[18]
+seed19 = "/artist/" + artists[19]
+seed20 = "/artist/" + artists[20]
+seed21 = "/artist/" + artists[21]
+seed22 = "/artist/" + artists[22]
 
 # main will handling the loop and crawling logic
 def main(startingArtist):
@@ -55,12 +101,38 @@ def scrapArtist(artistLink):
     html_soup = BeautifulSoup(response, 'html.parser')
     print(html_soup)
 
+    # Save response to file
+    with open("response.txt", "a") as file:
+        file.writelines(html_soup.prettify().encode('utf-8') + "; ")
+    
+
     # Append the artistlink to the array for future iterations
     nextLinksToCrawl.append(artistLink)
-
     return nextLinksToCrawl
 
 
 # run the main loop
-main(seed)
+main(seed0)
+main(seed1)
+main(seed2)
+main(seed3)
+main(seed4)
+main(seed5)
+main(seed6)
+main(seed7)
+main(seed8)
+main(seed9)
+main(seed10)
+main(seed11)
+main(seed12)
+main(seed13)
+main(seed14)
+main(seed15)
+main(seed16)
+main(seed17)
+main(seed18)
+main(seed19)
+main(seed20)
+main(seed21)
+main(seed22)
 driver.quit()
